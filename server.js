0d6e2a6cc9bbb58258ea754800aa55f005d63aad
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-09-02 16:12:56 -0400

    added express json

--- server.js
+++ server.js
@@ -9,3 +9,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+app.get('/notes', (req, res) => {
+    res.sendFile(__dirname + '/public/notes.html');
+  });
+  
+  app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+  });
+  
+  app.get('/api/notes', (req, res) => {
+    const data = JSON.parse(fs.readFileSync('db.json'));
+    res.json(data);
+  });
