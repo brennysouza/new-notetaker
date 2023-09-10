5e8e6abfcb0f90711012137f4f5dc549e28a9a57
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-09-09 21:37:13 -0400

    application finalized and fully functional

--- routes/notesapi.js
+++ routes/notesapi.js
@@ -1,37 +1,54 @@
 const router = require('express').Router();
 const fs = require('fs');
+const uuid = require('uuid');
+const routeNote = require('./routenotes');
 
+// Function to read data from the database JSON file
+function readDbNotes() {
+  const DbNotes = fs.readFileSync('db/db.json', 'utf-8');
+  return JSON.parse(DbNotes);
+}
+
+// Function to save data to the database JSON file
+function saveNoteDb(data) {
+  fs.writeFileSync('db/db.json', JSON.stringify(data, null, 2));
+}
+
+// Function to add a new note to the database
+function addDbNote(newNote) {
+  const existDbNote = readDbNotes();
+  existDbNote.push(newNote);
+  saveNoteDb(existDbNote);
+}
+
+// Function to remove a note from the database by ID
+function noteRemovalDb(uuid) {
+  const existDbNote = readDbNotes();
+  const filterThruNotes = existDbNote.filter((newNote) => {
+    return newNote.id !== uuid;
+  });
+  saveNoteDb(filterThruNotes);
+}
 
 router.get('/notes', function (req, res) {
-    console.log("Hello");
-    fs.readFile("db/db.json", "utf-8", (err, data) => {
-        console.log(data);
-        var dataJson = JSON.parse(data);
-        console.log(err);
-        return res.json(dataJson);
-    });
+  const dataJson = readDbNotes();
+  res.json(dataJson);
 });
 
 router.post('/notes', (req, res) => {
-    if (req.body && req.body.title && req.body.text) {
-        const newNote = new routeNote(req.body.title, req.body.text, uuid.v4())
-        addDbNote(newNote)
-        res.status(201).json(newNote);
-    } else {
-        res.status(400).json('Title and text required!');
-    }
-    // newNote.id = uuid.v4();
-    // const existingNotes = JSON.parse(fs.readFileSync('./db/db.json'));
-    // existingNotes.push(newNote);
-
-    // fs.writeFileSync('./db/db.json', JSON.stringify(existingNotes));
-    // res.json(newNote);
+  if (req.body && req.body.title && req.body.text) {
+    const newNote = new routeNote(req.body.title, req.body.text, uuid.v4());
+    addDbNote(newNote);
+    res.status(201).json(newNote);
+  } else {
+    res.status(400).json('Title and text required!');
+  }
 });
 
-//   Add delete route here as bonus
-router.delete("/notes/:id", (req, res) => {
-    noteRemovalDb(req.params.id)
-    res.status(200).send()
-  });
+router.delete('/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+  noteRemovalDb(noteId);
+  res.status(200).send();
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,55 +1,80 @@
+// Modules and libraries
 const express = require('express');
 const fs = require('fs');
 const path = require("path");
-const uuid = require('uuid');
+// const uuid = require('uuid');
+// Imports api routes
 const apiRoutes = require('./routes/notesapi');
-const routeNote = require('./routes/routenotes');
+// const routeNote = require('./routes/routenotes');
+// Defines the port
 const PORT = process.env.PORT || 3001;
+// Creates express application
 const app = express();
 
+// Middleware
+// Parse Json request bodies
 app.use(express.json());
+// Parse URL encoded request bodies
 app.use(express.urlencoded({ extended: true }));
+// Serve static files from the 'public' directory
 app.use(express.static('public'));
 
+// Function to read notes in the db file
+function readDbNotes() {
+  const DbNotes = fs.readFileSync('db/db.json', 'utf-8')
+  return JSON.parse(DbNotes)
+};
+
+// Function to save notes in database file
+function saveNoteDb(data) {
+  fs.writeFile('db/db.json', JSON.stringify(data, null, 2), (err) => {
+    if (err) {
+      console.error('error', err);
+    } else {
+      console.log('Note has been generated!');
+    }
+  });
+};
+
+// Function to add new notes in database file
+function addDbNote(newNote) {
+  const existDbNote = readDbNotes()
+  existDbNote.push(newNote)
+  saveNoteDb(existDbNote);
+};
+
+// Function to delete note from database file
+function noteRemovalDb(uuid) {
+  const existDbNote = readDbNotes()
+  const filterThruNotes = existDbNote.filter(newNote => {
+      return newNote.id !== uuid
+  })
+  saveNoteDb(filterThruNotes)
+};
+
+// Code uses the API routes defined in notesapi.js
 app.use('/api', apiRoutes);
 
+// Code that creates route for notes.html file.  
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
   });
-  
+
+// Code below defines a catch-all route to serve the 'index.html' page
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-  function readDbNotes() {
-    const DbNotes = fs.readFileSync('db/db.json', 'utf-8')
-    return JSON.parse(DbNotes)
-  };
-
-  function saveNoteDb(data) {
-    fs.writeFile('db/db.json', JSON.stringify(data, null, 2), (err) => {
-      if (err) {
-        console.error('error', err);
-      } else {
-        console.log('Note has been generated!');
-      }
-    });
-  };
-
-  function addDbNote(newNote) {
-    const existDbNote = readDbNotes()
-    existDbNote.push(newNote)
-    saveNoteDb(existDbNote);
-  };
-
-  function noteRemovalDb(uuid) {
-    const existDbNote = readDbNotes()
-    const filterThruNotes = existDbNote.filter(newNote => {
-        return newNote.id !== uuid
-    })
-    saveNoteDb(filterThruNotes)
-  };
-
+// This starts the server and listen on port defined above 3001
   app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
   });
+
+  // Code allows functions to be used in other modules
+  module.exports = {
+    readDbNotes,
+    saveNoteDb,
+    addDbNote,
+    noteRemovalDb,
+  };
+  
